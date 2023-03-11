501bc9591a6c7a6d2f766de7088388e566a17409
Nahid-07 <nahidasif1998@gmail.com>
2023-03-11 13:11:41 +0600

    server created

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const port = process.env.port || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+const uri =
+  "mongodb+srv://mascara-jwellary:tDT1C18xxq9qceDT@cluster0.ugpmzsn.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run(){
+    try{
+        const displayProduct = client.db("mascara-jwellary").collection("displayProduct");
+
+        app.get('/displayProduct', async(req,res)=>{
+            const query = {};
+            const data = await displayProduct.find(query).toArray()
+            res.send(data)
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.log(err))
+
+app.listen(port, () => {
+  console.log("port is listening");
+});
